c50418a8a44aa6ca90e856f6a05ca1eaeeef5d0f
Daniel Lim <hbt@markhamboards.com>
2017-10-02 21:49:56 -0400

    Update Video Pages  2
    Update Video Pages 2

--- video-contest/index.php
+++ video-contest/index.php
@@ -362,13 +362,13 @@ src="https://www.facebook.com/tr?id=295481444187735&ev=PageView&noscript=1"
           </div>
 		<?php /*
 		
-		<div class="social">
-			<a href="https://goo.gl/R3DtnP" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-			<a href="https://goo.gl/GFkRHZ" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-			<a href="https://goo.gl/e39SJM" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-			<a href="https://goo.gl/boQdhS" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-			<a href="https://goo.gl/Ruxo9a" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-		</div>
+			<div class="social">
+				<a href="https://goo.gl/R3DtnP" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/GFkRHZ" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/e39SJM" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/boQdhS" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/Ruxo9a" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+			</div>
 
 		*/ ?>
 			
