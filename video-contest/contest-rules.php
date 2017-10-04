ed71fa40cb045e3c0f0bfde5ca0bf69831431d8d
Daniel Lim <hbt@markhamboards.com>
2017-10-04 13:03:29 -0400

    clean up Video Contest Pages
    clean up Video Contest Pages

---
+++ video-contest/contest-rules.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+	<html class="ie ie7" lang="en-US">
+<![endif]-->
+<!--[if IE 8]>
+	<html class="ie ie8" lang="en-US">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8)  ]><!-->
+<html lang="en-US">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="The Awareness Project | Videos + Contest">
+  <meta name="author" content="Daniel | http://profoundideation.com">
+
+  <title>The Awareness Project Contest Rules</title>
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+
+  <!-- Bootstrap -->
+  <link href="http://bioprofitness.com/videos/assets/css/bootstrap.css" rel="stylesheet">
+  <link href="http://bioprofitness.com/videos/assets/css/bootstrap-theme.css" rel="stylesheet">
+  <link href="http://bioprofitness.com/videos/assets/css/font-awesome.css" rel="stylesheet">
+
+  <!-- simple styles -->
+  <link href="http://bioprofitness.com/videos/assets/css/style.css" rel="stylesheet">
+  <style type="text/css">
+
+  </style>
+
+  <!-- Source hidden -->
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-58506882-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-58506882-1');
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-2490173776997871",
+      enable_page_level_ads: true
+    });
+  </script>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+ fbq('init', '295481444187735'); 
+fbq('track', 'PageView');
+</script>
+<noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=295481444187735&ev=PageView
+&noscript=1"/>
+</noscript>
+<!-- End Facebook Pixel Code -->
+<!-- Facebook Pixel Code -->
+
+
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '295481444187735', {
+em: ''
+});
+fbq('track', 'PageView');
+
+	fbq("trackCustom", "Joined Project Awareness Campaign", {
+	});
+	
+  fbq('track', 'CompleteRegistration', {
+			value: 5.00,
+			currency: 'USD'
+	});			
+</script>
+  <noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=295481444187735&ev=PageView&noscript=1"
+/></noscript>
+  <!-- DO NOT MODIFY -->
+  <!-- End Facebook Pixel Code -->
+</head>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+  <div id="wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="jumbotron visibleBoxes">
+            <?php /* 
+							<div class="non-visibleBoxes">				
+						*/ ?>
+            
+            <h1 class="subtitle">Enter to Win a Round Trip for Two to the Caribbean!</h1>
+            
+            <div class="videoWrapperOuter">
+              <div class="videoWrapperInner">
+                <img src="http://bioprofitness.com/videos/assets/img/win-a-vacation.jpg" class="img-responsive" alt="Video Coming Soon | Responsive Image" />
+              </div>						
+            </div>
+            <h1>The Project "Awareness" Video Contest</h1>
+            <? /*
+<br />
+  <br />
+    <br />
+      <br />
+    <br />
+  <br />
+<br />
+*/ ?>
+
+Details Coming Soon
+
+
+          </div>
+          <!-- end jumbotron visibleBoxes -->
+
+          <div class="alert alert-danger" role="alert">
+            <p>
+              <h3><a href="#" class="vrlps-trigger" style="color:#ff0700;">Don't Miss the Next Video!</a></h3></p>
+          </div>
+          	<hr>
+          <div class="col-md-8 col-md-offset-2">
+            <div class="jumbotron visibleBoxes">
+              <br />
+              <div class="well visibleBoxes videoContest">
+                <div class="page-header">
+                  <h4>The Project "Awareness" Video Contest</h4>
+                </div>
+                <h5>Help us spread the word!</h5>
+                <div data-vl-widget="popupTrigger"></div>
+              </div>
+                <br />
+              <h3>Contest Ends on Nov. 2nd, 2017</h3>
+              <div id="countdown" style="color:#000;"></div>
+            </div>
+          </div>
+
+          </div>
+		<?php /*
+		
+			<div class="social">
+				<a href="https://goo.gl/R3DtnP" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/GFkRHZ" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/e39SJM" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/boQdhS" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+				<a href="https://goo.gl/Ruxo9a" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+			</div>
+
+		*/ ?>
+			
+      </div>
+    </div>
+
+    <div class="row">
+      	<br />
+      	<hr>
+      <div class="col-md-8 col-md-offset-2">
+        <div class="jumbotron visibleBoxes">
+          <div class="fb-comments" data-href="http://bioprofitness.com/videos/" data-width="500" data-numposts="5"></div>
+        </div>
+
+		<div class="social">
+			<a href="https://goo.gl/R3DtnP" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+			<a href="https://goo.gl/GFkRHZ" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+			<a href="https://goo.gl/e39SJM" target="_blank"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+			<a href="https://goo.gl/boQdhS" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+			<a href="https://goo.gl/Ruxo9a" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+		</div>
+
+
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-3">
+        <p class="copyright">&copy; BioProFitness - All Rights Reserved</p>
+        <div class="credits">
+          <!-- 
+          All the links in the footer should remain intact. 
+          You can delete the links only if you purchased the pro version.
+          Licensing information: https://bootstrapmade.com/license/
+          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=WeBuild
+          -->
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="http://bioprofitness.com/videos/assets/js/bootstrap.min.js"></script>
+  <script src="http://bioprofitness.com/videos/assets/js/jquery.countdown.min.js"></script>
+  <script type="text/javascript">
+    $("#countdown").countdown("2017/11/02", function(event) {
+      $(this).html(event.strftime("%w weeks %d days <br /> %H:%M:%S"));
+    });
+  </script>
+  
+	<script>!function(a,b,c,d,t){var e,f=a.getElementsByTagName("head")[0];if(!a.getElementById(c)){if(e=a.createElement(b),e.id=c,e.setAttribute("data-vrlps-ucid",d),e.setAttribute("data-vrlps-version","2"), e.setAttribute("data-vrlps-template", t),e.src="https://app.viral-loops.com/popup_assets/js/vl_load_v2.min.js",window.ub){jQuery=null,$=null;var g=a.createElement(b);g.src="https://code.jquery.com/jquery-2.2.4.min.js",f.appendChild(g)}f.appendChild(e);var h=a.createElement("link");h.rel="stylesheet",h.type="text/css",h.href="https://app.viral-loops.com/static/vl-loader.css",f.appendChild(h);var i=a.createElement("div");i.id="vl-overlay",i.style.display="none";var j=a.createElement("div");j.id="vl-loader",i.appendChild(j),a.addEventListener("DOMContentLoaded",function(b){a.body.appendChild(i);for(var c=a.getElementsByClassName("vrlps-trigger"),d=0;d<c.length;d++)c[d].removeAttribute("href"),c[d].onclick=function(){a.getElementById("vl-overlay").style.display="block"};var e=a.querySelectorAll("[data-vl-widget='popupTrigger']");[].forEach.call(e,function(b){var c=a.createElement("div");c.className="vl-embedded-cta-loader",b.appendChild(c)})})}}(document,"script","vrlps-js","45gbiRp2z3W09kyYcitIu9gVMXE","ranking");</script>
+	<script>
+  <script>
+    console.log("Someone else joined the campaign!");
+  </script>
+</body>
+
+</html>
