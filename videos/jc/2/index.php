f6d5f62468c398f3867e437601628dcb88e6133a
Daniel Lim <hbt@markhamboards.com>
2017-10-04 19:29:56 -0400

    template JC index
    template JC index

--- videos/jc/2/index.php
+++ videos/jc/2/index.php
@@ -360,11 +360,14 @@
                           frameborder="0" allowfullscreen></iframe>            
                       */ ?>         
                       
-                      <div class="fb-video" data-href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/"></a><p>Are &quot;NOOTROPICS&quot; Cheating?
+                        <div class="fb-video fb-videos" data-href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/"></a><p>Are &quot;NOOTROPICS&quot; Cheating?
 
-                      Like Our Facebook Page
-                      https://www.facebook.com/nootritionhub/</p>Posted by <a href="#" role="button">Jean-Charles Jr Mercedat</a> on Wednesday, October 4, 2017</blockquote>
-                      </div>
+                        Like Our Facebook Page
+                        https://www.facebook.com/nootritionhub/</p>Posted by <a href="#" role="button">Jean-Charles Jr Mercedat</a> on Wednesday, October 4, 2017</blockquote>
+                        </div>
+                        
+                        <div class="fb-videos">
+                        </div>
 
                       <?php /*
                         <div class="fb-video" data-href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" data-width="500" data-show-text="false">
