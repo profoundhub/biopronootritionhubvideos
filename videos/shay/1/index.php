468d35285e2b5e9c7902246e7fb1a37e81d12821
Daniel Lim <hbt@markhamboards.com>
2017-10-04 21:45:51 -0400

    Years ...
    9:45 PM 04/10/2017

--- videos/shay/1/index.php
+++ videos/shay/1/index.php
@@ -450,21 +450,7 @@ src="https://www.facebook.com/tr?id=295481444187735&ev=PageView&noscript=1"
 
 
 
-  
-  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-  <script src="http://bioprofitness.com/videos/assets/js/bootstrap.min.js"></script>
-  <script src="http://bioprofitness.com/videos/assets/js/jquery.countdown.min.js"></script>
-  <script type="text/javascript">
-    $("#countdown").countdown("2017/11/02", function(event) {
-      $(this).html(event.strftime("%w weeks %d days <br /> %H:%M:%S"));
-    });
-  </script>
-  
-	<script>!function(a,b,c,d,t){var e,f=a.getElementsByTagName("head")[0];if(!a.getElementById(c)){if(e=a.createElement(b),e.id=c,e.setAttribute("data-vrlps-ucid",d),e.setAttribute("data-vrlps-version","2"), e.setAttribute("data-vrlps-template", t),e.src="https://app.viral-loops.com/popup_assets/js/vl_load_v2.min.js",window.ub){jQuery=null,$=null;var g=a.createElement(b);g.src="https://code.jquery.com/jquery-2.2.4.min.js",f.appendChild(g)}f.appendChild(e);var h=a.createElement("link");h.rel="stylesheet",h.type="text/css",h.href="https://app.viral-loops.com/static/vl-loader.css",f.appendChild(h);var i=a.createElement("div");i.id="vl-overlay",i.style.display="none";var j=a.createElement("div");j.id="vl-loader",i.appendChild(j),a.addEventListener("DOMContentLoaded",function(b){a.body.appendChild(i);for(var c=a.getElementsByClassName("vrlps-trigger"),d=0;d<c.length;d++)c[d].removeAttribute("href"),c[d].onclick=function(){a.getElementById("vl-overlay").style.display="block"};var e=a.querySelectorAll("[data-vl-widget='popupTrigger']");[].forEach.call(e,function(b){var c=a.createElement("div");c.className="vl-embedded-cta-loader",b.appendChild(c)})})}}(document,"script","vrlps-js","45gbiRp2z3W09kyYcitIu9gVMXE","ranking");</script>
-	<script>
-  <script>
-    console.log("Someone new joined the campaign!");
-  </script>
+  <?php include('../../inc/footer.php'); ?>
+  <?php include('../../inc/countdown.php'); ?>
 </body>
-
 </html>
