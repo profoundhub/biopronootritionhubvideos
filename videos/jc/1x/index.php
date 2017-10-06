2461f9a0d08b3d467950702c3d9845f80c20b1be
Daniel Lim <hbt@markhamboards.com>
2017-10-06 14:56:28 -0400

    retro fitting
    retro fitting

--- videos/jc/1x/index.php
+++ videos/jc/1x/index.php
@@ -353,34 +353,11 @@
             <h1 class="subtitle">Are "NOOTROPICS" Cheating?</h1>
               <h2>Project "Awareness"</h2>
                 <br />
+                
                   <div class="videoWrapperOuter">
                     <div class="videoWrapperInner">
-                      <?php /*
-                        <iframe src="//www.youtube.com/embed/<------>?rel=0&amp;controls=0&amp;showinfo=0" 
-                          frameborder="0" allowfullscreen></iframe>            
-                      */ ?>         
-                      
-                        <div class="fb-video fb-videos" data-href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/"></a><p>Are &quot;NOOTROPICS&quot; Cheating?
-
-                        Like Our Facebook Page
-                        https://www.facebook.com/nootritionhub/</p>Posted by <a href="#" role="button">Jean-Charles Jr Mercedat</a> on Wednesday, October 4, 2017</blockquote>
-                        </div>
-                        
-                      <?php /*
-                        <div class="fb-video" data-href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" data-width="500" data-show-text="false">
-                          <blockquoteblockquote cite="https://www.facebook.com/jc.mercedat/videos/10159447689770224/" class="fb-xfbml-parse-ignore">
-                            <a href="https://www.facebook.com/jc.mercedat/videos/10159447689770224/"></a>
-                              <p>
-                                Are &quot;NOOTROPICS&quot; Cheating?                
-                                Like Our Facebook Page
-                                https://www.facebook.com/nootritionhub/
-                              </p>
-                                Posted by <a href="#" role="button">Jean-Charles Jr Mercedat</a> on Wednesday, October 4, 2017
-                          </blockquote>
-                        </div>
-                      */ ?>
-
-                      <?php /* <img src="http://bioprofitness.com/videos/assets/img/video-soon-overlay.jpg" class="img-responsive" alt="Video Coming Soon | Responsive Image" /> */ ?>
+                      <iframe src="//www.youtube.com/embed/7yfLtPDK_Ko?rel=0&amp;controls=0&amp;showinfo=0" 
+                      frameborder="0" allowfullscreen></iframe>                           
                     </div><!-- videoWrapperInner -->			
                   </div><!-- videoWrapperOuter --> 
                        
